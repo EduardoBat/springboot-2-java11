7481e8a1922cc45c2c993444d35974e3712096ed
Eduardo Batista <eduardobatista041994@gmail.com>
2020-04-15 17:58:47 -0300

    Many-To-ManyAssociation

--- src/main/java/com/example/course/entities/Product.java
+++ src/main/java/com/example/course/entities/Product.java
@@ -1,6 +1,5 @@
 package com.example.course.entities;
 
-
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
@@ -9,8 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
@@ -25,8 +26,9 @@ public class Product implements Serializable {
 	private Double price;
 	private String imgUrl;
 
-	@Transient
-	private Set<Category> categories = new HashSet<>();   //instanciar, para não comecar valendo nulo..mas sim vazia e instanciada 
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>(); // instanciar, para não comecar valendo nulo..mas sim vazia
 
 	public Product() {
 	}
